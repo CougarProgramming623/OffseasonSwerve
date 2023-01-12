038d2b46aff15a73995701fa25a97c232b5ad06d
SeungwooDY <sdyoon0701@gmail.com>
2023-01-12 18:34:33 -0500

    "first commit"

---
+++ OffseasonSwerveImported/src/main/cpp/SteerController.cpp
@@ -0,0 +1,45 @@
+#include "SteerController.h"
+#include "Constants.h"
+
+SteerController::SteerController(int motorID, int EncoderPort, double AngleOffset):
+    motor(motorID),
+    encoder{EncoderPort},
+    motorControlMode{ctre::phoenix::motorcontrol::ControlMode::Position}
+{
+    motor.SetSelectedSensorPosition(Deg2Rad(360-(fmod(((encoder.GetVoltage() * ENCODER_VOLTAGE_TO_DEGREE) + (360-AngleOffset)), 360))) / STEER_ENCODER_POSITION_CONSTANT);
+}
+
+double SteerController::GetReferenceAngle() {return referenceAngleRadians;}
+
+double SteerController::GetStateAngle(){ //gets the current angle of the motor
+    double motorAngleRadians = motor.GetSelectedSensorPosition() * STEER_ENCODER_POSITION_CONSTANT;
+    motorAngleRadians = fmod(motorAngleRadians, 2.0 * M_PI);
+    if(motorAngleRadians < 0.0){
+        motorAngleRadians += 2.0 * M_PI;
+    }
+    return motorAngleRadians;
+}
+
+void SteerController::SetReferenceAngle(double referenceAngleRadians){
+    double currentAngleRadians = motor.GetSelectedSensorPosition() * STEER_ENCODER_POSITION_CONSTANT;
+
+    double currentAngleRadiansMod = fmod(currentAngleRadians, (2.0 * M_PI));
+    if(currentAngleRadiansMod < 0.0){
+        currentAngleRadiansMod += 2.0 * M_PI;
+    }
+
+    // The reference angle has the range [0, 2pi) but the Falcon's encoder can go above that
+    double adjustedReferenceAngleRadians = referenceAngleRadians + currentAngleRadians - currentAngleRadiansMod;
+    if(referenceAngleRadians - currentAngleRadiansMod > M_PI){
+        adjustedReferenceAngleRadians -= 2.0 * M_PI;
+    } else if (referenceAngleRadians - currentAngleRadiansMod < -M_PI) {
+        adjustedReferenceAngleRadians += 2.0 * M_PI;
+    }
+
+    motor.Set(motorControlMode, ((adjustedReferenceAngleRadians / STEER_ENCODER_POSITION_CONSTANT))); //FIX maybe subtract a zero angle instead
+    //referenceAngleRadians = referenceAngleRadians; //FIX maybe is assigned the adjusted reference angle?
+    // DebugOutF("angle: " + std::to_string((adjustedReferenceAngleRadians)/*-(currentAngleRadians)*/));
+    // DebugOutF("angleDeg: " + std::to_string( Rad2Deg((adjustedReferenceAngleRadians)/*-(currentAngleRadians)*/)));
+    // DebugOutF("TarEnc: " + std::to_string((adjustedReferenceAngleRadians / STEER_ENCODER_POSITION_CONSTANT)/*-(currentAngleRadians/STEER_ENCODER_POSITION_CONSTANT)*/));
+    // DebugOutF("CurEnc: " + std::to_string(motor.GetSelectedSensorPosition()));
+}
---
+++ OffseasonSwerveImported/src/main/cpp/subsystems/Drivetrain.cpp
@@ -0,0 +1,56 @@
+#include "subsystems/Drivetrain.h"
+
+#include <frc/trajectory/Trajectory.h>
+#include <frc/kinematics/SwerveModuleState.h>
+
+DriveTrain::DriveTrain()
+    : m_FrontLeftLocation(units::meter_t (DRIVETRAIN_TRACKWIDTH_METERS / 2.0), units::meter_t (DRIVETRAIN_WHEELBASE_METERS / 2.0)),
+      m_FrontRightLocation(units::meter_t (DRIVETRAIN_TRACKWIDTH_METERS / 2.0), units::meter_t (-DRIVETRAIN_WHEELBASE_METERS / 2.0)),
+      m_BackLeftLocation(units::meter_t (-DRIVETRAIN_TRACKWIDTH_METERS / 2.0), units::meter_t (DRIVETRAIN_WHEELBASE_METERS / 2.0)),
+      m_BackRightLocation(units::meter_t (-DRIVETRAIN_TRACKWIDTH_METERS / 2.0), units::meter_t (-DRIVETRAIN_WHEELBASE_METERS / 2.0)),
+      m_Kinematics(m_FrontLeftLocation, m_FrontRightLocation, m_BackLeftLocation, m_BackRightLocation),
+      m_Rotation(),
+      m_FrontLeftModule(FRONT_LEFT_MODULE_DRIVE_MOTOR, FRONT_LEFT_MODULE_STEER_MOTOR, FRONT_LEFT_MODULE_ENCODER_PORT, -137.3),
+      m_FrontRightModule(FRONT_RIGHT_MODULE_DRIVE_MOTOR, FRONT_RIGHT_MODULE_STEER_MOTOR, FRONT_RIGHT_MODULE_ENCODER_PORT, 73),
+      m_BackLeftModule(BACK_LEFT_MODULE_DRIVE_MOTOR, BACK_LEFT_MODULE_STEER_MOTOR, BACK_LEFT_MODULE_ENCODER_PORT, -140.3),
+      m_BackRightModule(BACK_RIGHT_MODULE_DRIVE_MOTOR, BACK_RIGHT_MODULE_STEER_MOTOR, BACK_RIGHT_MODULE_ENCODER_PORT, -1),
+      m_ChassisSpeeds{0_mps, 0_mps, 0_rad_per_s}
+{}
+
+void DriveTrain::BaseDrive(double power){
+  //DebugOutF("FR= " + std::to_string(Rad2Deg(m_FrontRightModule.m_SteerController.GetStateAngle())));
+  //DebugOutF("BL= " + std::to_string(Rad2Deg(m_BackLeftModule.m_SteerController.GetStateAngle())));
+  //DebugOutF("BR= " + std::to_string(Rad2Deg(m_BackRightModule.m_SteerController.GetStateAngle())));
+  //DebugOutF("FL= " + std::to_string(Rad2Deg(m_FrontLeftModule.m_SteerController.GetStateAngle())));
+  
+  // auto [fl, fr, bl, br] = m_Kinematics.ToSwerveModuleStates(m_ChassisSpeeds);
+  // frc::SwerveModuleState states[4] = {fl, fr, bl, br};
+  //m_Kinematics.DesaturateWheelSpeeds(fl, fr, bl, br);
+  double angle = 90;
+  //DebugOutF("CurEncBD: " + std::to_string(m_FrontLeftModule.m_SteerController.motor.GetSelectedSensorPosition()));
+  // m_BackRightModule.Set(0, angle);
+  // m_BackLeftModule.Set(0, angle);
+  // m_FrontLeftModule.Set(0, angle);
+  // m_FrontRightModule.Set(0, angle);
+
+
+  // m_FrontLeftModule.Set(states[0].speed / kMAX_VELOCITY_METERS_PER_SECOND * kMAX_VOLTAGE, (double) states[0].angle.Radians());
+  // m_FrontRightModule.Set(states[1].speed / kMAX_VELOCITY_METERS_PER_SECOND * kMAX_VOLTAGE, (double) states[1].angle.Radians());
+  // m_BackLeftModule.Set(states[2].speed / kMAX_VELOCITY_METERS_PER_SECOND * kMAX_VOLTAGE, (double) states[2].angle.Radians());
+  // m_BackRightModule.Set(states[3].speed / kMAX_VELOCITY_METERS_PER_SECOND * kMAX_VOLTAGE, (double) states[3].angle.Radians());
+}
+void DriveTrain::DriveInit(){}
+
+void DriveTrain::BreakMode(bool on){
+  m_FrontLeftModule.BreakMode(true);
+  m_FrontRightModule.BreakMode(true);
+  m_BackLeftModule.BreakMode(true);
+  m_BackRightModule.BreakMode(true);
+}
+
+// void DriveTrain::UseVelocityPID(){}
+// void DriveTrain::UseMagicPID(){}
+// void DriveTrain::UsePostionPID(){}
+// void DriveTrain::SetPID(double E, double P, double I, double D, double F){}
+
+// void DriveTrain::DriveToPosition(double x){}
